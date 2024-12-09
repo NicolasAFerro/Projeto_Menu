14fa5f754f78eef1b3eeffbd4e172bbf4bb03add
NicolasAFerro <nicolasalexandrinoferro@gmail.com>
2024-12-08 21:00:15 -0300

    feat: função calcular

--- javascript.js
+++ javascript.js
@@ -1,5 +1,51 @@
 $("#telefone").mask("(99) 99999-9999");
 
+var produtos = [
+  { id: 1, name: "Bife com Batata", price: 30.0 },
+  { id: 2, name: "Coxa de Frango Crocante", price: 25.0 },
+  { id: 3, name: "Carne de Panela", price: 22.0 },
+  { id: 4, name: "Farofa", price: 10.0 },
+  { id: 5, name: "Salada", price: 8.0 },
+  { id: 6, name: "Torresmo", price: 12.0 },
+];
+
+function calc() {
+  var nome = null;
+  nome = document.getElementById("nome");
+  var saida = null;
+
+  var formatter = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+
+  var arrayCardsSum = document.getElementsByName("quantity");
+  saida = document.getElementById("resultado");
+  var totalCompra = 0;
+  var stringSaida = "";
+  saida.innerHTML = "";
+  stringSaida += `Caro ${nome.value} <br><br> Seguem os dados do seu pedido  <br><br>`;
+  stringSaida += `O seu pedido é: <br><br>`;
+  stringSaida += "<ul>";
+  for (var i = 0; i < arrayCardsSum.length; i++) {
+    if (arrayCardsSum[i].value > 0) {
+      var id = arrayCardsSum[i].id;
+      var produto = produtos[id - 1].name;
+      var precoUn = produtos[id - 1].price;
+      var total = precoUn * arrayCardsSum[i].value;
+      stringSaida += `<li>Prato: ${produto} - `;
+      stringSaida += `Preço unitário=${formatter.format(precoUn)} - `;
+      stringSaida += `Quantidade ${arrayCardsSum[i].value} - `;
+      stringSaida += `Total: ${total}</li>`;
+      totalCompra += total;
+    }
+  }
+  stringSaida += `<br><br><strong>Preço total ${formatter.format(
+    totalCompra
+  )}</strong>`;
+  saida.innerHTML = stringSaida;
+}
+
 //função do card de aumentar quantidade
 function wcqib_refresh_quantity_increments() {
   jQuery(
